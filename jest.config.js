103a07284c514e29895024a290b73c797c12c8df
Anton Samarin <Anton.Samarin@jetbrains.com>
2023-08-02 11:00:35 +0200

    Modified config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    rootDir: '/home/Anton.Samarin/Desktop/issues/2023.2/Code\ coverage/project/',
+    rootDir: '/home/Anton.Samarin/Desktop/issues/2023.2/Code\ coverage/code-coverage/code-coverage/',
     moduleFileExtensions: ['ts', 'html', 'js', 'json'],
-    coverageDirectory: '/home/Anton.Samarin/Desktop/issues/2023.2/Code\ coverage/output',
+    coverageDirectory: '/home/Anton.Samarin/Desktop/issues/2023.2/Code\ coverage/code-coverage/output',
     coverageReporters: ['lcov'],
-    collectCoverageFrom: ['**/sum.js', 
-    '**/subtract.js', 
-    '**/divide.js']
+    collectCoverageFrom: ['src/sum.js', 
+    'src/subtract.js', 
+    'src/divide.js']
 };
