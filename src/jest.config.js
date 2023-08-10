02788eb3dd8f98fac994e5a18c39243ecce6b78e
Anton Samarin <Anton.Samarin@jetbrains.com>
2023-08-10 11:49:57 +0200

    Updated the coverage config once more

--- src/jest.config.js
+++ src/jest.config.js
@@ -3,10 +3,10 @@ module.exports = {
     transform: {
         '\\.[tj]s$': 'babel-jest',
     },
+    rootDir:'/';
     coverageDirectory: '.qodana/code-coverage/',
     coverageReporters: ['lcovonly'],
     collectCoverageFrom: ['src/subtract.js', 
     'src/sum.js', 
     'src/divide.js']
-
 };
