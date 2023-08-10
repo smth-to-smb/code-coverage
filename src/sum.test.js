c5dcc05ddb198d6440555fe3a409d31015232a4f
Anton Samarin <Anton.Samarin@jetbrains.com>
2023-08-10 13:35:56 +0200

    Fourth

--- src/sum.test.js
+++ src/sum.test.js
@@ -11,6 +11,6 @@ test('dels 10 - 8 to equal 2', () => {
 const divide = require('./divide');
 
 test('divides 6 by 2 to equal 3', () => {
-  expect(divide.divide(6, 2)).toBe(3);
+  expect(divide(6, 2)).toBe(3);
 });
 
