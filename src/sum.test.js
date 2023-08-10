57e44f46d574239436f4376ac293a637663145c5
Anton Samarin <Anton.Samarin@jetbrains.com>
2023-08-10 13:33:51 +0200

    Another fix attempt

--- src/sum.test.js
+++ src/sum.test.js
@@ -8,9 +8,9 @@ test('dels 10 - 8 to equal 2', () => {
   expect(sum.del(10, 8)).toBe(2);
 });
 
-const sum = require('./divide');
+const divide = require('./divide');
 
 test('divides 6 by 2 to equal 3', () => {
-  expect(sum.sum(6, 2)).toBe(3);
+  expect(divide.divide(6, 2)).toBe(3);
 });
 
